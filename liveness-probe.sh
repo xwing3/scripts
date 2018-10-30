4c19716421894ee3f076e75d0af118a9c74966d6
Florian M <madarflorin@gmail.com>
2018-10-30 22:44:27 +0200

    Update liveness-probe.sh

--- liveness-probe.sh
+++ liveness-probe.sh
@@ -1,3 +1,5 @@
+# Magento liveness probe shell script for Kubernetes when nginx and magento are in the same container
+
 #!/bin/bash
 response=$(curl -sL -w "%{http_code}\\n" --header "Host: "$HOST_MAGENTO"" $HOSTNAME:8080/index.php -o /dev/null);
 if [[ "$response" -gt 199 && "$response" -lt 400 ]] ; then echo HTTP Code: $response; unset response; exit 0; else echo HTTP Code: $response; exit 127; fi
